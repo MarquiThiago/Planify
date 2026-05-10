9955abc65b9037bca8ada772701dbbe67c24ca8e
Thiago <thiago.smarqui21@gmail.com>
2026-05-10 16:05:22 -0300

    feat(auth) create login page

---
+++ lib/src/features/auth/presentation/pages/login_page.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../../../core/di/injection.dart';
+import '../bloc/auth_bloc.dart';
+import '../bloc/auth_state.dart';
+import '../widgets/login_error_widget.dart';
+import '../widgets/login_initial_widget.dart';
+import '../widgets/login_loading_widget.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (_) => getIt<AuthBloc>(),
+      child: const Scaffold(
+        body: SafeArea(
+          child: _LoginBody(),
+        ),
+      ),
+    );
+  }
+}
+
+class _LoginBody extends StatelessWidget {
+  const _LoginBody();
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<AuthBloc, AuthState>(
+      builder: (context, state) {
+        return switch (state) {
+          AuthInitial() => const LoginInitialWidget(),
+          AuthLoading() => const LoginLoadingWidget(),
+          AuthError(:final message) => LoginErrorWidget(message: message),
+        };
+      },
+    );
+  }
+}
