25216da30e5f358ebd061f71ad3ddbeb4379b9bd
Thiago <thiago.smarqui21@gmail.com>
2026-05-21 20:09:51 -0300

    feat(transactions): fix inejction

--- lib/src/core/di/injection.config.dart
+++ lib/src/core/di/injection.config.dart
@@ -24,16 +24,20 @@ import 'package:planify/src/features/auth/domain/repository/auth_repository.dart
 import 'package:planify/src/features/auth/modulo/auth_module.dart' as _i288;
 import 'package:planify/src/features/auth/presentation/bloc/auth_bloc.dart'
     as _i399;
-import 'package:planify/src/features/transactions/domain/repository/category_repository.dart'
-    as _i238;
-import 'package:planify/src/features/transactions/domain/repository/transaction_repository.dart'
-    as _i539;
-import 'package:planify/src/features/transactions/modulo/transaction_module.dart'
-    as _i129;
-import 'package:planify/src/features/transactions/presentation/bloc/create_transaction/create_transaction_bloc.dart'
-    as _i742;
-import 'package:planify/src/features/transactions/presentation/bloc/transaction_bloc.dart'
-    as _i1026;
+import 'package:planify/src/features/transactions/features/create_transaction/domain/repository/category_repository.dart'
+    as _i598;
+import 'package:planify/src/features/transactions/features/create_transaction/domain/repository/create_transaction_repository.dart'
+    as _i166;
+import 'package:planify/src/features/transactions/features/create_transaction/modulo/create_transaction_module.dart'
+    as _i559;
+import 'package:planify/src/features/transactions/features/create_transaction/presentation/bloc/create_transaction_bloc.dart'
+    as _i465;
+import 'package:planify/src/features/transactions/features/get_transactions/domain/repository/get_transaction_repository.dart'
+    as _i550;
+import 'package:planify/src/features/transactions/features/get_transactions/modulo/get_transaction_module.dart'
+    as _i363;
+import 'package:planify/src/features/transactions/features/get_transactions/presentation/bloc/transaction_bloc.dart'
+    as _i1060;
 import 'package:supabase_flutter/supabase_flutter.dart' as _i454;
 
 extension GetItInjectableX on _i174.GetIt {
@@ -45,13 +49,16 @@ extension GetItInjectableX on _i174.GetIt {
     final gh = _i526.GetItHelper(this, environment, environmentFilter);
     final accountModule = _$AccountModule();
     final authModule = _$AuthModule();
-    final transactionModule = _$TransactionModule();
+    final createTransactionModule = _$CreateTransactionModule();
+    final getTransactionModule = _$GetTransactionModule();
     final coreModule = _$CoreModule();
     gh.factory<_i494.AccountBloc>(() => accountModule.accountBloc);
     gh.factory<_i399.AuthBloc>(() => authModule.authBloc);
-    gh.factory<_i1026.TransactionBloc>(() => transactionModule.transactionBloc);
-    gh.factory<_i742.CreateTransactionBloc>(
-      () => transactionModule.createTransactionBloc,
+    gh.factory<_i465.CreateTransactionBloc>(
+      () => createTransactionModule.createTransactionBloc,
+    );
+    gh.factory<_i1060.TransactionBloc>(
+      () => getTransactionModule.transactionBloc,
     );
     gh.lazySingleton<_i454.SupabaseClient>(() => coreModule.supabaseClient);
     gh.lazySingleton<_i712.AuthCubit>(() => coreModule.authCubit);
@@ -59,11 +66,14 @@ extension GetItInjectableX on _i174.GetIt {
       () => accountModule.accountRepository,
     );
     gh.lazySingleton<_i129.AuthRepository>(() => authModule.authRepository);
-    gh.lazySingleton<_i539.TransactionRepository>(
-      () => transactionModule.transactionRepository,
+    gh.lazySingleton<_i166.CreateTransactionRepository>(
+      () => createTransactionModule.createTransactionRepository,
     );
-    gh.lazySingleton<_i238.CategoryRepository>(
-      () => transactionModule.categoryRepository,
+    gh.lazySingleton<_i598.CategoryRepository>(
+      () => createTransactionModule.categoryRepository,
+    );
+    gh.lazySingleton<_i550.GetTransactionRepository>(
+      () => getTransactionModule.getTransactionRepository,
     );
     return this;
   }
@@ -73,6 +83,8 @@ class _$AccountModule extends _i913.AccountModule {}
 
 class _$AuthModule extends _i288.AuthModule {}
 
-class _$TransactionModule extends _i129.TransactionModule {}
+class _$CreateTransactionModule extends _i559.CreateTransactionModule {}
+
+class _$GetTransactionModule extends _i363.GetTransactionModule {}
 
 class _$CoreModule extends _i258.CoreModule {}
