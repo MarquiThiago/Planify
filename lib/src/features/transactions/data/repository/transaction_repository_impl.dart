30b8fa072357aaa9f8324a9b92f48584486f5351
Thiago <thiago.smarqui21@gmail.com>
2026-05-20 21:08:44 -0300

    feat(transactions): create the transaction repository with query

---
+++ lib/src/features/transactions/data/repository/transaction_repository_impl.dart
@@ -0,0 +1,31 @@
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+import '../../domain/entities/transaction_entity.dart';
+import '../../domain/repository/transaction_repository.dart';
+import '../models/transaction_model.dart';
+
+class TransactionRepositoryImpl implements TransactionRepository {
+  TransactionRepositoryImpl(this._client);
+
+  final SupabaseClient _client;
+
+  @override
+  Future<List<TransactionEntity>> getTransactions(int year, int month) async {
+    final firstDay =
+        '${year.toString()}-${month.toString().padLeft(2, '0')}-01';
+    final lastDayOfMonth = DateTime(year, month + 1, 0).day;
+    final lastDay =
+        '${year.toString()}-${month.toString().padLeft(2, '0')}-${lastDayOfMonth.toString().padLeft(2, '0')}';
+
+    final response = await _client
+        .from('transactions')
+        .select('*, categories(name, color), accounts(name)')
+        .gte('date', firstDay)
+        .lte('date', lastDay)
+        .order('date', ascending: false);
+
+    return response
+        .map((json) => TransactionModel.fromJson(json).toEntity())
+        .toList();
+  }
+}
